301add16e3dd27f0b25749cbe3465ce41fa6f64e
丁宗江 <114076268+dingzongjiang@users.noreply.github.com>
2023-05-23 12:19:47 +0800

    完善了RegisterMainActivity.java的逻辑，创建了user类，时间是2023-05-22-11:28

--- app/src/main/java/com/example/internetprojectpractice/pojo/User.java
+++ app/src/main/java/com/example/internetprojectpractice/pojo/User.java
@@ -1,5 +1,134 @@
 package com.example.internetprojectpractice.pojo;
 
-public class User {
+import android.content.Intent;
 
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+
+    private String gender;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String email, String password, String phoneNumber, String gender) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+    }
+
+    /**
+     * 获取
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * 设置
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * 获取
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取
+     * @return phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * 设置
+     * @param phoneNumber
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * 获取
+     * @return gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * 设置
+     * @param gender
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
 }
+
